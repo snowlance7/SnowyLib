6da0c190bc6fab18629f99ae8f961f3a141174ed
snowlance7 <znowlance@gmail.com>
2026-05-10 17:02:57 -0500

    added autodictionary

---
+++ AutoDictionary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnowyLib
+{
+    public class AutoDictionary<TKey, TValue> : Dictionary<TKey, TValue>
+    where TValue : new()
+    {
+        public new TValue this[TKey key]
+        {
+            get
+            {
+                if (!ContainsKey(key))
+                    base[key] = new TValue();
+
+                return base[key];
+            }
+            set => base[key] = value;
+        }
+    }
+}
