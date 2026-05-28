691c0fd5d14b28109c60529a92391752b20c12b7
snowlance7 <znowlance@gmail.com>
2026-05-27 19:56:36 -0500

    push

---
+++ Extensions/ComponentExtensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SnowyLib.Extensions
+{
+    internal static class ComponentExtensions
+    {
+        public static bool TryGetComponentInChildren<T>(this Component comp, out T? component) where T : Component
+        {
+            component = comp.GetComponentInChildren<T>();
+            return component != null;
+        }
+    }
+}
--- Extensions/GameObjectExtensions.cs
+++ Extensions/GameObjectExtensions.cs
@@ -8,5 +8,11 @@ namespace SnowyLib
         {
             return gameObject.TryGetComponent(out StatusEffectController controller) ? controller : gameObject.AddComponent<StatusEffectController>();
         }
+
+        public static bool TryGetComponentInChildren<T>(this GameObject go, out T? component) where T : Component
+        {
+            component = go.GetComponentInChildren<T>();
+            return component != null;
+        }
     }
 }
