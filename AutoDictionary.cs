5205b5b4e8f14a18d12373f1b5349d02647902c0
snowlance7 <znowlance@gmail.com>
2026-06-03 20:04:31 -0500

    push

--- AutoDictionary.cs
+++ AutoDictionary.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SnowyLib
 {
     public class AutoDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TValue : new()
     {
+        private readonly Func<TKey, TValue> _factory;
+
+        public AutoDictionary(Func<TKey, TValue> factory)
+        {
+            _factory = factory;
+        }
+
         public new TValue this[TKey key]
         {
             get
             {
-                if (!ContainsKey(key))
-                    base[key] = new TValue();
+                if (!TryGetValue(key, out TValue value))
+                {
+                    value = _factory(key);
+                    base[key] = value;
+                }
 
-                return base[key];
+                return value;
             }
             set => base[key] = value;
         }
