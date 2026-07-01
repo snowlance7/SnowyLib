f75c807159587170d066469952b1646ab067cf27
snowy <znowlance@gmail.com>
2026-07-01 14:20:47 -0500

    push

--- Interfaces/SingletonItemInterface.cs
+++ Interfaces/SingletonItemInterface.cs
@@ -6,15 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
+using static SnowyLib.Plugin;
 
 namespace SnowyLib
 {
     public interface ISingletonItem { } // TODO: Test this
 
     [HarmonyPatch]
-    internal class ISingletonItemPatch
+    internal static class ISingletonItemPatch
     {
         private static HashSet<Type> spawned = new HashSet<Type>();
+        private static GrabbableObject? despawningDuplicate;
 
         [HarmonyPostfix]
         [HarmonyPatch(typeof(GrabbableObject), nameof(GrabbableObject.InitializeAfterPositioning))]
@@ -28,17 +30,18 @@ namespace SnowyLib
 
                 if (spawned.Contains(type))
                 {
-                    var spawnableScrap = RoundManager.Instance.currentLevel.spawnableScrap.Select(x => x.rarity).ToList();
+                    var spawnableScrap = RoundManager.Instance.currentLevel.spawnableScrap.Where(x => x.spawnableItem != __instance.itemProperties).ToList();
+                    var spawnableScrapRarities = spawnableScrap.Select(x => x.rarity).ToList();
 
-                    int randomWeightedIndexList = RoundManager.Instance.GetRandomWeightedIndexList(spawnableScrap);
-                    Item replacementItem = RoundManager.Instance.currentLevel.spawnableScrap[randomWeightedIndexList].spawnableItem;
+                    int randomWeightedIndexList = RoundManager.Instance.GetRandomWeightedIndexList(spawnableScrapRarities);
+                    Item replacementItem = spawnableScrap[randomWeightedIndexList].spawnableItem;
                     int replacementItemValue = (int)(UnityEngine.Random.Range(replacementItem.minValue, replacementItem.maxValue) * RoundManager.Instance.scrapValueMultiplier);
+                    logger.LogDebug($"Only one {__instance.itemProperties.name} instance can be spawned, replacing duplicate with {replacementItem.name} with a scrap value of {replacementItemValue}");
 
-                    var vector = __instance.transform.position + Vector3.up * 0.25f;
-                    GrabbableObject spawnedReplacementItem = Utils.SpawnItem(__instance.itemProperties.GetDawnInfo().TypedKey, vector)!;
-                    spawnedReplacementItem.startFallingPosition = vector;
-                    spawnedReplacementItem.targetFloorPosition = spawnedReplacementItem.GetItemFloorPosition(__instance.transform.position);
-                    spawnedReplacementItem.NetworkObject.Spawn();
+                    var spawnPos = __instance.transform.position + Vector3.up * 0.25f;
+                    GrabbableObject spawnedReplacementItem = Utils.SpawnItem(replacementItem.GetDawnInfo().TypedKey, spawnPos)!;
+                    //spawnedReplacementItem.startFallingPosition = spawnPos;
+                    //spawnedReplacementItem.targetFloorPosition = spawnedReplacementItem.GetItemFloorPosition(spawnedReplacementItem.transform.position);
 
                     IEnumerator SetScrapSpawnOnNetworkSpawn(GrabbableObject grabbableObject, int value)
                     {
@@ -49,6 +52,7 @@ namespace SnowyLib
 
                     Plugin.Instance.StartCoroutine(SetScrapSpawnOnNetworkSpawn(spawnedReplacementItem, replacementItemValue));
 
+                    despawningDuplicate = __instance;
                     __instance.NetworkObject.Despawn(destroy: true);
                 }
                 else
@@ -56,8 +60,9 @@ namespace SnowyLib
                     spawned.Add(type);
                 }
             }
-            catch
+            catch (Exception e)
             {
+                logger.LogError(e);
                 return;
             }
         }
@@ -69,9 +74,9 @@ namespace SnowyLib
             try
             {
                 if (!__instance.IsServer || __instance is not ISingletonItem) { return; }
+                if (despawningDuplicate != null && despawningDuplicate == __instance) { despawningDuplicate = null; return; }
 
-                Type type = __instance.GetType();
-                spawned.Remove(type);
+                spawned.Remove(__instance.GetType());
             }
             catch
             {
