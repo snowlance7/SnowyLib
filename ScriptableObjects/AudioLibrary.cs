9f1c14d342778f9009e2d5accf571644427ab31b
snowy <znowlance@gmail.com>
2026-07-31 18:10:53 -0500

    ready for 1.10.0

---
+++ ScriptableObjects/AudioLibrary.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SnowyLib
+{
+    [CreateAssetMenu(fileName = "SnowyLib", menuName = "SnowyLib/AudioLibrary")]
+    public class AudioLibrary : ScriptableObject
+    {
+        public AudioClipGroup[] Groups = [];
+        public AudioClip[] Clips = [];
+
+        public AudioClip? GetClip(string clipName)
+        {
+            return Clips.Where(x => x.name.ToLower() == clipName.ToLower()).FirstOrDefault();
+        }
+
+        public AudioClip[]? GetClips(string groupId)
+        {
+            var group = Groups.Where(x => x.Id.ToLower() == groupId.ToLower()).FirstOrDefault();
+            return group != null ? group.Clips : null;
+        }
+    }
+
+    [System.Serializable]
+    public class AudioClipGroup
+    {
+        public string Id = "";
+        public AudioClip[] Clips = [];
+    }
+}
