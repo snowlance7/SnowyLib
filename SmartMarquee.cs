860d2ba140a0bad4c3ca541dd3aa0e766fabc57f
snowy <znowlance@gmail.com>
2026-08-16 17:44:01 -0500

    ready for 1.13.3

---
+++ SmartMarquee.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace SnowyLib
+{
+    public class SmartMarquee : MonoBehaviour
+    {
+        RectTransform _transform, _parent;
+
+        [SerializeField] float _stoppedTime = 1f, _movingTime = 2f;
+
+        [SerializeField] bool snapRight;
+
+        Coroutine? _currentAnim;
+
+        void Awake()
+        {
+            _transform = GetComponent<RectTransform>();
+            _parent = transform.parent.GetComponent<RectTransform>();
+        }
+
+        void Update()
+        {
+            if (_parent.rect.width < _transform.rect.width)
+            {
+                if (_currentAnim != null)
+                    return;
+
+                _transform.anchoredPosition = new Vector2(_transform.sizeDelta.x / 2, _transform.anchoredPosition.y);
+                _currentAnim = StartCoroutine(DoAnimation());
+            }
+            else if (_currentAnim != null)
+            {
+                StopCoroutine(_currentAnim);
+                _currentAnim = null;
+                _transform.localPosition = Vector3.zero;
+            }
+        }
+
+        IEnumerator DoAnimation()
+        {
+            // wait X seconds
+            yield return new WaitForSeconds(_stoppedTime);
+
+            float t = 0;
+            Vector3 step = Vector3.left * (_transform.rect.width - _parent.rect.width) / _movingTime;
+
+            // move left for Y seconds
+            while (t < _movingTime)
+            {
+                t += Time.deltaTime;
+
+                _transform.localPosition += step * Time.deltaTime;
+                yield return null;
+            }
+
+            // wait for X seconds
+            yield return new WaitForSeconds(_stoppedTime);
+
+            if (!snapRight)
+            {
+                // move right for Y seconds
+                while (t > 0)
+                {
+                    t -= Time.deltaTime;
+
+                    _transform.localPosition -= step * Time.deltaTime;
+                    yield return null;
+                }
+            }
+
+            _currentAnim = null;
+        }
+    }
+}
