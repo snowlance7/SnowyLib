b7258c3fba95900dff2ee6866e8f61e62113cfb0
snowy <znowlance@gmail.com>
2026-09-13 13:00:29 -0500

    ready for 1.16.3

--- SmartMarqueeVertical.cs
+++ SmartMarqueeVertical.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 
 namespace SnowyLib
 {
-    public class SmartMarquee : MonoBehaviour
+    public class VerticalSmartMarquee : MonoBehaviour
     {
         RectTransform _transform = null!, _parent = null!;
 
         [SerializeField] float _stoppedTime = 1f, _movingTime = 2f;
 
-        [SerializeField] bool snapRight;
+        [SerializeField] bool snapBottom;
 
         Coroutine? _currentAnim;
 
@@ -21,12 +21,12 @@ namespace SnowyLib
 
         void Update()
         {
-            if (_parent.rect.width < _transform.rect.width)
+            if (_parent.rect.height < _transform.rect.height)
             {
                 if (_currentAnim != null)
                     return;
 
-                _transform.anchoredPosition = new Vector2(_transform.sizeDelta.x / 2, _transform.anchoredPosition.y);
+                _transform.anchoredPosition = new Vector2(_transform.anchoredPosition.x, (_transform.rect.height - _parent.rect.height) / 2f);
                 _currentAnim = StartCoroutine(DoAnimation());
             }
             else if (_currentAnim != null)
@@ -39,32 +39,37 @@ namespace SnowyLib
 
         IEnumerator DoAnimation()
         {
-            // wait X seconds
+            // Wait X seconds
             yield return new WaitForSeconds(_stoppedTime);
 
             float t = 0;
-            Vector3 step = Vector3.left * (_transform.rect.width - _parent.rect.width) / _movingTime;
 
-            // move left for Y seconds
+            float distance = _transform.rect.height - _parent.rect.height;
+
+            Vector3 step = Vector3.down * distance / _movingTime;
+
+            // Move up for Y seconds
             while (t < _movingTime)
             {
                 t += Time.deltaTime;
 
                 _transform.localPosition += step * Time.deltaTime;
+
                 yield return null;
             }
 
-            // wait for X seconds
+            // Wait for X seconds
             yield return new WaitForSeconds(_stoppedTime);
 
-            if (!snapRight)
+            if (!snapBottom)
             {
-                // move right for Y seconds
+                // Move back down for Y seconds
                 while (t > 0)
                 {
                     t -= Time.deltaTime;
 
                     _transform.localPosition -= step * Time.deltaTime;
+
                     yield return null;
                 }
             }
